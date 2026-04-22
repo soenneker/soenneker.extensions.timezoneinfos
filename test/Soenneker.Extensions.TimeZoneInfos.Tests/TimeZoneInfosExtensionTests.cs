889e24f6288c3b7f9eb6b200191349dfdde2405d
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:02:06 -0500

    TUnit conversion

--- test/Soenneker.Extensions.TimeZoneInfos.Tests/TimeZoneInfosExtensionTests.cs
+++ test/Soenneker.Extensions.TimeZoneInfos.Tests/TimeZoneInfosExtensionTests.cs
@@ -1,11 +1,10 @@
-﻿using Soenneker.Tests.Unit;
-using Xunit;
+using Soenneker.Tests.Unit;
 
 namespace Soenneker.Extensions.TimeZoneInfos.Tests;
 
 public sealed class TimeZoneInfosExtensionTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
